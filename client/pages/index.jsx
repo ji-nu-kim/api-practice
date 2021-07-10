934810ebb80dce6b3fc4265224e4e8212e2bec9a
ji-nu-kim <wlsdn5153@naver.com>
2021-07-11 00:32:01 +0900

    first commit

---
+++ client/pages/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import MsgList from '../components/MsgList';
+
+const Home = () => {
+  return <>
+    <h1>Simple SNS</h1>
+    <MsgList/>
+  </>
+}
+
+export default Home;
