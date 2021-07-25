3032e58a0d3be684043dfab3deaa706bb514f102
ji-nu-kim <wlsdn5153@naver.com>
2021-07-25 23:31:21 +0900

    graphql 서버세팅

---
+++ server/src/resolvers/message.js
@@ -0,0 +1,56 @@
+import { v4 } from 'uuid';
+import { writeDB } from '../dbController.js';
+
+const setMsgs = data => writeDB('messages', data);
+/*
+  parent: parent 객체. 거의 사용하지 않음
+  args: Query에 필요한 필드에 제공되는 인수(parameter)
+  context: 로그인한 사용자. DB Access 등의 중요한 정보들
+*/
+
+const messageResolver = {
+  // get
+  Query: {
+    messages: (parent, args, { db }) => {
+      return db.messages;
+      // console.log({parent, args, conatext})
+    },
+    message: (parent, { id = '' }, { db }) => {
+      return db.messages.find(msg => msg.id === id);
+    },
+  },
+  // post, delete, put, patch
+  Mutation: {
+    createMessage: (parent, { text, userId }, { db }) => {
+      const newMsg = {
+        id: v4(),
+        text,
+        userId,
+        timestamp: Date.now(),
+      };
+
+      db.messages.unshift(newMsg);
+      setMsgs(db.messages);
+      return newMsg;
+    },
+    updateMessage: (parent, { id, userId, text }, { db }) => {
+      const targetIndex = db.messages.findIndex(msg => msg.id === id);
+      if (targetIndex < 0) throw Error('메시지가 없습니다');
+      if (db.messages[targetIndex].userId !== userId) throw Error('사용자가 다릅니다');
+      const newMsg = { ...db.messages[targetIndex], text };
+      db.messages.splice(targetIndex, 1, newMsg);
+      setMsgs(db.messages);
+      return newMsg;
+    },
+    deleteMessage: (parent, { id, userId }, { db }) => {
+      const targetIndex = db.messages.findIndex(msg => msg.id === id);
+      if (targetIndex < 0) throw Error('메시지가 없습니다');
+      if (db.messages[targetIndex].userId !== userId) throw Error('사용자가 다릅니다');
+      db.messages.splice(targetIndex, 1);
+      setMsgs(db.messages);
+      return id;
+    },
+  },
+};
+
+export default messageResolver;
---
+++ server/src/resolvers/user.js
@@ -0,0 +1,9 @@
+const userResolver = {
+  Query: {
+    users: (parent, args, { db }) => Object.values(db.users),
+    user: (parent, { id }, { db }) => db.users[id],
+  },
+  Mutation: {},
+};
+
+export default userResolver;
